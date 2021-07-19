a6a4342135ab268fcea80182fbd20f85e1d6e20f
Your Name <you@example.com>
2021-07-19 23:43:00 +0000

    sdsdg

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ node {
 
         stage('Download Repo') {
              sh '''terraform init
-                terraform destroy --auto-approve'''
+                terraform apply --auto-approve'''
              }
 
 
