91141bcbb092d03bce8c19a06a24a77d1259e380
Your Name <you@example.com>
2021-07-19 21:17:34 +0000

    sdgh

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,10 @@
 node {
+    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '2'))])
+
+    
     timestamps {
 
     sh 'echo \'Hello World\''
-    
+
 }
 }
